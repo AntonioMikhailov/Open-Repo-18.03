a5b1c3d67ab554746c0e84a426af3ee45bf54a23
Anton <antoniom@mail.ru>
2022-03-18 15:55:36 +0300

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+// "use strict";
+//  let num = 5;
+//  console.log(num);
+
+
+let name = 'Ann';
+let lastName = 'Ivanova';
+
+console.log("Имя девушки " +name + " а фамилия " +lastName ); // приходится делать отступы от кавычек для пробелов
+console.log(`Имя девушки ${name} а фамилия ${lastName}`); // вариант с интерполяцией Бэктиками
